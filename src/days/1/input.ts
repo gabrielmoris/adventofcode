09b7f75de0fe9b7f2d6666c6f46c3fe0e610996a
gabrielcmoris <gabrielcmoris@gmail.com>
2024-12-04 02:40:48 -0500

    adapt template

--- src/days/1/input.ts
+++ src/days/1/input.ts
@@ -1,2 +1,1 @@
-export const arg1 =
-  "This is the argument. You can use a maximum of 2 arguments: input.ts for the main input and input2.ts (optional) for additional input.";
+export const arg1 = `This is the argument. You can use a maximum of 2 arguments: input.ts for the main input and input2.ts (optional) for additional input.`;
