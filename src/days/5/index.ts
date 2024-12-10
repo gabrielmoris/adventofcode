44d75fb33a8ee320a4748033ba5de2e3f03e365f
gabriel <gabrielcmoris@gmail.com>
2024-12-10 22:24:53 +0100

    start logic first challenge

--- src/days/5/index.ts
+++ src/days/5/index.ts
@@ -1,17 +1,23 @@
-export const solutionFn = ({ arg }: { arg: string }) => {
-  const pageOrdering = arg
+export const solutionFn = ({ test }: { test: string }) => {
+  let sumOfMiddlePages = 0;
+  const pageOrdering = test
     .split("\n\n")[0]
     .split("\n")
-    .map((strpage) => {
-      return strpage.split("|").map((str) => Number(str));
-    });
+    .map((strpage) => strpage.split("|").map((str) => Number(str)));
 
-  const pages = arg
+  const pages = test
     .split("\n\n")[1]
     .split("\n")
-    .map((line) => line.split(",").map((str) => Number(str)));
+    .map((page) => page.split(",").map((item) => Number(item)));
 
-  return;
+  for (let i = 0; i < pages.length; i++) {
+    let isCorrect = true;
+    pages[i].forEach((page) => {});
+
+    if (isCorrect) sumOfMiddlePages += pages[i][(pages[i].length - 1) / 2];
+  }
+
+  return sumOfMiddlePages;
 };
 
 export const solutionFn2 = ({ arg }: { arg: string }) => {};
