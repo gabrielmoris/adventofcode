149bbe402cac6495bccec5a7b3afedb466b6f2d1
gabrielcmoris <gabrielcmoris@gmail.com>
2024-12-11 06:24:06 -0500

    first part day 5

--- src/days/5/index.ts
+++ src/days/5/index.ts
@@ -1,23 +1,36 @@
-export const solutionFn = ({ test }: { test: string }) => {
+export const solutionFn = ({ arg }: { arg: string }) => {
   let sumOfMiddlePages = 0;
-  const pageOrdering = test
-    .split("\n\n")[0]
-    .split("\n")
-    .map((strpage) => strpage.split("|").map((str) => Number(str)));
+  const [orderingRules, updatesList] = arg.split("\n\n");
 
-  const pages = test
-    .split("\n\n")[1]
-    .split("\n")
-    .map((page) => page.split(",").map((item) => Number(item)));
+  const rules = new Map();
+  orderingRules.split("\n").forEach((rule) => {
+    const [before, after] = rule.split("|").map(Number);
+    if (!rules.has(after)) rules.set(after, new Set());
+    rules.get(after).add(before);
+  });
 
-  for (let i = 0; i < pages.length; i++) {
+  const updates = updatesList
+    .split("\n")
+    .map((update) => update.split(",").map(Number));
+
+  updates.forEach((update) => {
     let isCorrect = true;
-    pages[i].forEach((page) => {});
+    for (let i = 0; i < update.length; i++) {
+      for (let j = i + 1; j < update.length; j++) {
+        if (!rules.get(update[j])?.has(update[i])) {
+          isCorrect = false;
+        }
+      }
+      if (!isCorrect) break;
+    }
 
-    if (isCorrect) sumOfMiddlePages += pages[i][(pages[i].length - 1) / 2];
-  }
+    if (isCorrect) {
+      const middleIndex = Math.floor(update.length / 2);
+      sumOfMiddlePages += update[middleIndex];
+    }
+  });
 
   return sumOfMiddlePages;
 };
 
-export const solutionFn2 = ({ arg }: { arg: string }) => {};
+export const solutionFn2 = ({ test }: { test: string }) => {};
